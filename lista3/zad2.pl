6f4d14abeef610bf8beb7cd518af725b2bebe93c
Lukasz Mazurkiewicz <gepird@gmail.com>
2018-03-22 07:57:11 +0100

    ładniejsze podejście

--- lista3/zad2.pl
+++ lista3/zad2.pl
@@ -5,7 +5,7 @@ maximum([X|L],M1,M):-
     (X>M1-> M2 is X; M2 is M1),
     maximum(L,M2,M).
 
-jestTablicaMaxow([], [], _).
+jestTablicaMaxow([], [0], _).
 jestTablicaMaxow([X|Lista], [MaxDlaX|Maxy], BiezacaSuma):-
     Y is BiezacaSuma+X,
     (Y<0 ->
@@ -17,8 +17,4 @@ jestTablicaMaxow([X|Lista], [MaxDlaX|Maxy], BiezacaSuma):-
 
 max_sum(Lista, Suma):-
     jestTablicaMaxow(Lista, Maxy, 0),
-    maximum(Maxy, Suma2),
-    (Suma2<0 ->
-        Suma=0;
-        Suma=Suma2
-    ).
+    maximum(Maxy, Suma).
